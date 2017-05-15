359eb522158004233520aca5c1995aae2683fa09
Denis Dudarev <dudarev.d@dbbest.com>
2017-05-15 13:43:37 +0300

    Fix routing

---
+++ src/AppBundle/Listener/ReqListener.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: denis
+ * Date: 15.05.17
+ * Time: 13:20
+ */
+
+namespace AppBundle\Listener;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+//use Symfony\Component\HttpFoundation\Response;
+
+class ReqListener
+{
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        if ($event->getException() instanceof NotFoundHttpException) {
+            $response = new RedirectResponse('storeRequest/first');
+            $event->setResponse($response);
+        }
+    }
+
+
+}
